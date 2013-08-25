1afc658a6bb726576f4078ea9ddaf651185f9453
Rasmus Andersson <rasmus@notion.se>
2013-08-25 11:28:41 -0700

    css zoom level generator update

--- _scripts/gen-css-size-levels.js
+++ _scripts/gen-css-size-levels.js
@@ -1,4 +1,4 @@
-var start = 900;
+var start = 320;
 var end = 2000+1;
 var base = 900; // at zoom:1
 var step = 1;
@@ -6,6 +6,6 @@ var width;
 
 for (width = start; width != end; width += step) {
   console.log('@media screen and (min-width: '+width+'px) { '+
-              'body { zoom: '+(width/base)+'; } '+
+              'body { zoom: '+(width/base).toFixed(3)+'; } '+
               '}');
 }
